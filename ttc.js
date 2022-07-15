f523ceabdc5ac64e2050d9e40ed00b02e5088b02
Dillon M <dillon32300@gmail.com>
2022-07-15 15:12:39 -0500

    second commit

--- ttc.js
+++ ttc.js
@@ -68,18 +68,7 @@
     }
 
 
-    //defining variables that queryselect
-    let zero = document.getElementById('0')
-    let one = document.getElementById('1')
-    let two = document.getElementById('2')
-    let three = document.getElementById('3')
-    let four = document.getElementById('4')
-    let five = document.getElementById('5')
-    let six = document.getElementById('6')
-    let seven = document.getElementById('7')
-    let eight = document.getElementById('8')
     
-    //attempting to calculate win for first row
     
 
    
